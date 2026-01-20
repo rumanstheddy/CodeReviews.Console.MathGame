d815ad2e615ce75632c1275e4c0ff77133389879
Sumanth Reddy <sumanthcob@gmail.com>
2026-01-19 21:24:14 -0500

    Refactor GameRound and GameSession to generate questions dynamically and remove static question dictionary

--- GameRound.cs
+++ GameRound.cs
@@ -1,18 +1,57 @@
 class GameRound
 {
-    public (int, int)[] Questions { get; set; }
     public char Operation { get; }
     public int TotalScore { get; set; }
+    private readonly int _questionsPerRound = 10;
+    public int QuestionsPerRound
+    {
+        get { return _questionsPerRound; }
+    }
+    private readonly (int, int)[] _questions;
+    public IReadOnlyList<(int, int)> Questions
+    {
+        get { return _questions; }
+    }
     private readonly int[] _answers;
     public IReadOnlyList<int> Answers
     {
         get { return _answers; }
     }
+    private void GenerateQuestionsForOperation()
+    {
+        Random rnd = new();
+        var uniqueQuestions = new HashSet<(int, int)>();
+
+        int i = 0;
+        while (i < _questionsPerRound)
+        {
+            int a = rnd.Next(1, 101);
+            int b = rnd.Next(1, 101);
+            if (Operation == '-')
+            {
+                b = rnd.Next(1, a + 1);
+            }
+            else if (Operation == '/')
+            {
+                while (a % b != 0)
+                {
+                    b = rnd.Next(2, a);
+                }
+            }
+
+            var question = (a, b);
+            if (uniqueQuestions.Add(question))
+            {
+                _questions[i] = question;
+                i += 1;
+            }
+        }
+    }
     private void ComputeAnswersForOperation()
     {
-        for (int i = 0; i < Questions.Length; i++)
+        for (int i = 0; i < _questions.Length; i++)
         {
-            (int, int) question = Questions[i];
+            (int, int) question = _questions[i];
             int answer = Operation switch
             {
                 '+' => question.Item1 + question.Item2,
@@ -25,11 +64,12 @@ class GameRound
             _answers[i] = answer;
         }
     }
-    public GameRound(char operation, (int, int)[] questions)
+    public GameRound(char operation)
     {
         Operation = operation;
-        _answers = [.. new int[questions.Length]];
-        Questions = questions;
+        _questions = [.. new (int, int)[_questionsPerRound]];
+        GenerateQuestionsForOperation();
+        _answers = [.. new int[_questionsPerRound]];
         ComputeAnswersForOperation();
     }
 }
--- GameSession.cs
+++ GameSession.cs
@@ -12,15 +12,6 @@ class GameSession
         {4, '/'},
     }.ToImmutableDictionary();
 
-    // TODO: Move away from static questions (maybe from a csv file?)
-    public static readonly ImmutableDictionary<char, (int, int)[]> QuestionsDictionary = new Dictionary<char, (int, int)[]>
-    {
-        {'+', [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16), (17, 18), (19, 20)]},
-        {'-', [(10, 2), (15, 5), (20, 7), (18, 9), (12, 3), (22, 11), (30, 10), (25, 8), (40, 20), (50, 25)]},
-        {'*', [(2, 3), (4, 5), (6, 2), (7, 3), (8, 4), (5, 5), (9, 2), (3, 6), (10, 3), (4, 7)]},
-        {'/', [(8, 2), (15, 3), (18, 6), (20, 4), (21, 7), (24, 6), (30, 5), (36, 6), (40, 8), (27, 3)]},
-    }.ToImmutableDictionary();
-
     public void ShowMenu()
     {
         if (_isFirstRound)
@@ -68,11 +59,11 @@ class GameSession
         int option = int.Parse(Console.ReadLine()!);
         char operation = OptionsDictionary[option];
 
-        GameRound gameRound = new(operation, QuestionsDictionary[operation]);
+        GameRound gameRound = new(operation);
 
         Console.WriteLine();
         Console.WriteLine("You've selected: " + operation);
-        for (int i = 0; i < gameRound.Questions.Length; i++)
+        for (int i = 0; i < gameRound.Questions.Count; i++)
         {
             (int, int) q = gameRound.Questions[i];
             Console.WriteLine();
